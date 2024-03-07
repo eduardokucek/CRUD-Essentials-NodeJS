112531c93156d4ac85d0b54580ca7b93d4608108
Eduardo <eduardo.kucek@gmail.com>
2024-03-07 18:07:48 -0300

    02 - 2111-5346 - zero ao NodeJS Rocketseat

--- server.js
+++ server.js
@@ -8,12 +8,62 @@
 
 // server.listen(3333);
 
+// Fastify framework for Node.js
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
 const server = fastify();
 
-server.get("/", () => {
-  return "Server is running at PORT 3333";
+const database = new DatabaseMemory();
+
+// HTTP METHODS
+// POST
+server.post("/videos", (request, response) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return response.status(201).send("Video created successfully");
+});
+
+// GET - all
+server.get("/videos", (request, response) => {
+  const search = request.query.search;
+
+  const videos = database.list(search);
+
+  return videos;
+});
+
+// GET - by ID
+server.get("/videos/:id", (request, response) => {
+  return `GET method with id: ${request.params.id}`;
+});
+
+// PATCH
+server.patch("/videos/:id", (request, response) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return response.status(204).send("Video updated successfully");
+});
+
+// DELETE
+server.delete("/videos/:id", (request, response) => {
+  const videoId = request.params.id;
+  database.delete(videoId);
+
+  return response.status(204).send("Video deleted successfully");
 });
 
 server.listen({
